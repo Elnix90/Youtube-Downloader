a237f6917c847255c1a17ce65be0290881bac254
Elnix90 <elnix9@gmail.com>
2025-10-10 21:18:30 +0200

    small random changes + modified how add_new_ids work

--- FUNCTIONS/process_all.py
+++ FUNCTIONS/process_all.py
@@ -134,8 +134,6 @@ def process_all(
         print(f"[PROCESSING] Processing {total_videos} videos...")
     logger.info(f"[PROCESSING] Processing {total_videos} videos...")
 
-    raise
-
     for video_id in video_ids:
 
         start_processing: float = time.time()
