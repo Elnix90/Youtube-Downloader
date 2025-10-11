a28e0abded55b24172488581b60b85be81a9c0f5
Elnix90 <elnix91@gmail.com>
2025-10-11 17:32:31 +0200

    Added docstrings

---
+++ update_playlist.py
@@ -0,0 +1,17 @@
+"""
+Adds the liked videos to a custom playlist defined in config.py
+"""
+from constants import CONFIG
+from DEBUG.add_videos_to_playlist import add_videos
+
+playlist_id = CONFIG['other']['music_playlist_id']
+
+
+if __name__ == "__main__" and playlist_id is not None:
+    add_videos(
+        playlist_id,
+        CONFIG['other']["clean"],
+        CONFIG['processing']["test_run"],
+        CONFIG['processing']["info"],
+        CONFIG['processing']["error"],
+    )
