63078e62a4e9d12657986be97625c5e88b8b9db4
Elnix90 <elnix91@gmail.com>
2025-10-11 17:06:51 +0200

    improved updating date

--- DEBUG/update_date_added.py
+++ DEBUG/update_date_added.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from FUNCTIONS.HELPERS.fileops import load
 from FUNCTIONS.HELPERS.fprint import fprint
 from FUNCTIONS.HELPERS.logger import setup_logger
-from FUNCTIONS.sql_requests import commit_changes_to_db
+from FUNCTIONS.sql_requests import commit_changes_to_db, get_videos_in_db
 
 logger = setup_logger(__name__)
 
@@ -25,7 +25,14 @@ def update_date_added(
 
     Displays progress and estimated time remaining (ETA).
     """
-    video_ids = load(playlist_file)
+    video_ids_map = load(playlist_file)
+    video_ids = list(video_ids_map.keys())
+
+    vids_in_db = get_videos_in_db(True, cur)
+
+    for vid in vids_in_db:
+        if vid not in video_ids:
+            video_ids.append(vid)
 
     updated_ids: int = 0
     errored_ids: int = 0
@@ -37,7 +44,7 @@ def update_date_added(
 
     start_time = time.time()
 
-    for index, video_id in enumerate(video_ids.keys()):
+    for index, video_id in enumerate(video_ids):
         try:
             _ = cur.execute(
                 """
@@ -45,7 +52,7 @@ def update_date_added(
                 SET date_added = ?
                 WHERE video_id = ?
                 """,
-                (time.time(), video_id),
+                (start_time + 5 * index, video_id),
             )
             updated_ids += 1
         except sqlite3.OperationalError:
@@ -53,7 +60,7 @@ def update_date_added(
 
         # Compute ETA
         elapsed = time.time() - start_time
-        avg_time = elapsed / index
+        avg_time = elapsed / (index + 1) if not index else index
         remaining = total_videos - index
         eta_seconds = remaining * avg_time
         eta_formatted = str(timedelta(seconds=int(eta_seconds)))
@@ -64,13 +71,11 @@ def update_date_added(
             f"Updated {updated_ids}, failed {errored_ids}",
         )
 
-        time.sleep(0.1)
-
     _ = commit_changes_to_db(conn, True)
 
     total_duration = str(timedelta(seconds=int(time.time() - start_time)))
     fprint(
-        "[DONE]",
+        "[DONE] | ",
         f"Updated {updated_ids}/{total_videos} videos " + f"(failed: {errored_ids}) in {total_duration}",
     )
     logger.info(
