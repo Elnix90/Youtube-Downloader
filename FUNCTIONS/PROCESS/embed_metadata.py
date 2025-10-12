5b442f4a7f0db0edcc558edc45758253c627d02c
Elnix90 <elnix91@gmail.com>
2025-10-12 10:23:33 +0200

    Removed date functionnality, working on something similar that will work better

--- FUNCTIONS/PROCESS/embed_metadata.py
+++ FUNCTIONS/PROCESS/embed_metadata.py
@@ -9,14 +9,9 @@ from sqlite3 import Cursor
 
 # from DEBUG.compare_dicts import compare_dicts
 from FUNCTIONS.HELPERS.fprint import fprint
-from FUNCTIONS.HELPERS.helpers import (
-    VideoInfo,
-    normalize_skips,
-    remove_data_from_video_info,
-    timestamp_to_id3_unique,
-)
+from FUNCTIONS.HELPERS.helpers import VideoInfo, normalize_skips, remove_data_from_video_info
 from FUNCTIONS.HELPERS.logger import setup_logger
-from FUNCTIONS.metadata import get_metadata_tag, read_id3_tag, write_id3_tag
+from FUNCTIONS.metadata import get_metadata_tag, write_id3_tag
 from FUNCTIONS.sql_requests import get_video_info_from_db
 
 logger = setup_logger(__name__)
@@ -29,7 +24,7 @@ def embed_metadata_for_video(
     test_run: bool,
     cur: Cursor,
     info: bool,
-    force_update_date: bool,
+    # force_update_date: bool,
     force_update_metadata: bool
 ) -> tuple[float, bool]:
     """
@@ -40,42 +35,46 @@ def embed_metadata_for_video(
     start_processing: float = time.time()
 
     video_info: VideoInfo = get_video_info_from_db(video_id=video_id, cur=cur)
-    date: float = video_info.get('date_added', 0.0)
-    tm: str = timestamp_to_id3_unique(date, True)
+    # date: float = video_info.get('date_added', 0.0)
+    # tm: str = timestamp_to_id3_unique(date, True)
     title: str = video_info.get("title", "")
 
     if info:
         fprint(progress_prefix, "Embedding metadata for ?", title)
     logger.verbose(f"[Metadata] Embedding metadata for '{title}'")
 
+    # -----------------------------------------------
+    # SCREW IT IT DOESN'T WORK ON MY MUSIC PLAYER
+    # -----------------------------------------------
+
     # Embed the id in the dat field, to sort the video in the player
 
-    file_date, state = read_id3_tag(filepath=filepath, frame_id="TDRC")
+    # file_date, state = read_id3_tag(filepath=filepath, frame_id="TDRC")
 
-    update_date: bool = force_update_date
-    if state == 0:
-        if isinstance(file_date, list):
-            file_date = str(file_date[0])
-        else:
-            file_date = str(file_date)
-        if file_date != tm:
-            update_date = True
+    # update_date: bool = force_update_date
+    # if state == 0:
+    #     if isinstance(file_date, list):
+    #         file_date = str(file_date[0])
+    #     else:
+    #         file_date = str(file_date)
+    #     if file_date != tm:
+    #         update_date = True
 
-    else:
-        update_date = True
+    # else:
+    #     update_date = True
 
-    if update_date:
-        success_date: bool = True
-        if date:
-            success_date = write_id3_tag(filepath=filepath, frame_id="TDRC", data=tm, test_run=test_run)
-            if not success_date:
-                logger.warning(f"[Metadata] Failed to embed date '{tm}' for '{title}'")
-            else:
-                if info:
-                    fprint(progress_prefix, f"Embedded date '{tm}' for ?", title)
-                logger.info(f"[Metadata] Embedded date '{tm}' for '{title}'")
-    else:
-        fprint(progress_prefix, "No need to change date, skipping")
+    # if update_date:
+    #     success_date: bool = True
+    #     if date:
+    #         success_date = write_id3_tag(filepath=filepath, frame_id="TDRC", data=tm, test_run=test_run)
+    #         if not success_date:
+    #             logger.warning(f"[Metadata] Failed to embed date '{tm}' for '{title}'")
+    #         else:
+    #             if info:
+    #                 fprint(progress_prefix, f"Embedded date '{tm}' for ?", title)
+    #             logger.info(f"[Metadata] Embedded date '{tm}' for '{title}'")
+    # else:
+    #     fprint(progress_prefix, "No need to change date, skipping")
 
     # Embed full metadata as JSON
 
